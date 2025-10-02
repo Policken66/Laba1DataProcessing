d6aa0522e24ddd92fc0144896cd8a6e50b26b505
Artem Komlev <a.komlev.work@gmail.com>
2025-10-02 18:18:40 +0700

    Добавлены данные по Democracy Index.

---
+++ DemocracyIndex/democracy_index_processing.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+
+import pandas as pd
+from pandas import Series
+
+
+def convert_csv2exel(csv_path: Path, excel_path: Path):
+    df = pd.read_csv(csv_path, sep=',')
+    df.to_excel(excel_path)
+
+def select_inform_about_russia(path: Path) -> Series:
+    df = pd.read_excel(path)
+    result = df[df.apply(lambda row: row.astype(str).str.contains('Russia').any(), axis=1)]
+    return result
+
+def save_in_file(path: Path, data: Series):
+    data.to_excel(path)
--- run.py
+++ run.py
@@ -1,10 +1,22 @@
 from pathlib import Path
 
-from FreedomHouse.freedom_house_processing import select_inform_about_russia, save_in_file
+from DemocracyIndex import democracy_index_processing
+from FreedomHouse import freedom_house_processing
 
 if __name__ == "__main__":
-    print("Hello")
     input_path = Path("FreedomHouse/Resources/freedom_house.xlsx")
-    data = select_inform_about_russia(input_path)
+    data = freedom_house_processing.select_inform_about_russia(input_path)
     output_path = Path("FreedomHouse/Output/freedom_house.xlsx")
-    save_in_file(path=output_path, data=data)
+    freedom_house_processing.save_in_file(path=output_path, data=data)
+
+    csv_path_EIU_DI = Path("DemocracyIndex/Resources/EIU_DI.csv")
+    csv_path_EIU_DI_DATADICT = Path("DemocracyIndex/Resources/EIU_DI_DATADICT.csv")
+    csv_path_EIU_DI_WIDEF = Path("DemocracyIndex/Resources/EIU_DI_WIDEF.csv")
+
+    exel_path_EIU_DI = Path("DemocracyIndex/Output/EIU_DI.xlsx")
+    exel_path_EIU_DI_DATADICT = Path("DemocracyIndex/Output/EIU_DI_DATADICT.xlsx")
+    exel_path_EIU_DI_WIDEF = Path("DemocracyIndex/Output/EIU_DI_WIDEF.xlsx")
+
+    democracy_index_processing.convert_csv2exel(csv_path_EIU_DI, exel_path_EIU_DI)
+    democracy_index_processing.convert_csv2exel(csv_path_EIU_DI_DATADICT, exel_path_EIU_DI_DATADICT)
+    democracy_index_processing.convert_csv2exel(csv_path_EIU_DI_WIDEF, exel_path_EIU_DI_WIDEF)
